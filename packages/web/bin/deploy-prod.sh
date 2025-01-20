cf181d18861a08ff9a212cbda3078acdb10e9ee6
wit <witwitchayakarn@gmail.com>
2025-01-20 13:14:02 +0700

    Fix aws s3 sync

--- packages/web/bin/deploy-prod.sh
+++ packages/web/bin/deploy-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-aws s3 sync out/_next s3://augurrank-web/_next --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/_next s3://augurrank-web/_next --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out/nextImageExportOptimizer s3://augurrank-web/nextImageExportOptimizer --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/nextImageExportOptimizer s3://augurrank-web/nextImageExportOptimizer --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out s3://augurrank-web --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --size-only --delete --cache-control max-age=86400 --profile justnote
+aws s3 sync out s3://augurrank-web --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --delete --cache-control max-age=86400 --profile justnote
 
 aws cloudfront create-invalidation --distribution-id E11UOUS0F79ED8 --paths "/*" --profile justnote
--- packages/web/bin/deploy-test.sh
+++ packages/web/bin/deploy-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-aws s3 sync out/_next s3://test-augurrank-web/_next --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/_next s3://test-augurrank-web/_next --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out/nextImageExportOptimizer s3://test-augurrank-web/nextImageExportOptimizer --size-only --delete --cache-control max-age=31536000 --profile justnote
+aws s3 sync out/nextImageExportOptimizer s3://test-augurrank-web/nextImageExportOptimizer --size-only --cache-control max-age=31536000 --profile justnote
 
-aws s3 sync out s3://test-augurrank-web --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --size-only --delete --cache-control max-age=86400 --profile justnote
+aws s3 sync out s3://test-augurrank-web --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --delete --cache-control max-age=86400 --profile justnote
 
 aws cloudfront create-invalidation --distribution-id E3BW20Z9Q1C4V9 --paths "/*" --profile justnote
