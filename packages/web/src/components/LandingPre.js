d11504fee7fd50d38475e388e07d00d79a52e291
wit <witwitchayakarn@gmail.com>
2025-02-13 11:42:56 +0700

    Set images above fold priority true

--- packages/web/src/components/LandingPre.js
+++ packages/web/src/components/LandingPre.js
@@ -93,7 +93,7 @@ export function LandingPre() {
     <main className="relative mx-auto max-w-2xl overflow-hidden px-4 py-20 sm:px-6 lg:px-8 xl:px-12">
       <div className="flex flex-col items-center justify-start sm:flex-row sm:justify-center">
         <div className="rounded-full border-2 border-slate-800 p-2">
-          <Image className="size-32" src={Logo} alt="" />
+          <Image className="size-32" src={Logo} alt="" placeholder="empty" priority={true} />
         </div>
         <div className="mt-6 sm:ml-6 sm:mt-0">
           <h1 className="text-center text-5xl font-medium text-white sm:text-left">AugurRank</h1>
